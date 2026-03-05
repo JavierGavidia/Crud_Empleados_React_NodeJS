06aa0a53dd7133a30c27e6a8fa86b6ef863bec88
Javier Gavidia <javiergavidia@gmail.com>
2026-03-05 18:37:27 +0100

    Proyecto con la arquitectura preparada para comenzar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+import './styles/main.scss';
 
 function App() {
 
   return (
     <>
 
-      <h1>Registro de Empleados</h1>
+      <div className='pagina pag-inicio'>
+        <h1></h1>
+        <div className="content-btns">
+          <button className='btn-primary'><p>Acceder</p></button>
+          <button className='btn-secondary'><p>Cancelar</p></button>
+        </div>
+        <p className="texto">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus magnam quia repudiandae distinctio delectus, possimus ipsum sapiente fugit expedita. Rem id earum, consequuntur voluptatum minima quo reprehenderit aperiam sapiente magnam!
+          Cumque nam nulla totam, nesciunt saepe nihil nisi modi a recusandae reiciendis, possimus porro nobis, atque fugiat provident. Id quo quis placeat nulla animi praesentium nihil voluptatem dolores repellendus facere?</p>
+
+      </div>
 
     </>
   )
